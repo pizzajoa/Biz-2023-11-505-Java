393dcffaaf41e02b213826a84da4bd536762d65e
pizzajoa <jjh0954@naver.com>
2023-11-27 11:50:46 +0900

    오전수업

--- Java_001/src/com/callor/hello/algorithm/LoopL.java
+++ Java_001/src/com/callor/hello/algorithm/LoopL.java
@@ -4,10 +4,10 @@ public class LoopL {
 	public static void main(String[] args) {
 		int ranage = 100;
 		int sum = 0;
-		for (int i = 0; i < ranage ; i++) {
-			
-			if (i % 2 == 0){
-			sum += (i+2);
+		for (int i = 0; i < ranage; i++) {
+
+			if (i % 2 == 0) {
+				sum += (i + 2);
 			}
 		}
 		System.out.println("결과 : " + sum);
